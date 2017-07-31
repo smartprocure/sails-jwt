ffb1a832a160748424f680b0df7cfe9dadc88246
Sean Clayton <sclayton@smartprocure.us>
2017-07-31 14:06:00 -0400

    Implement sails-async

--- server/AuthController.js
+++ server/AuthController.js
@@ -1,8 +1,9 @@
 let _ = require('lodash/fp')
 let JWT = require('./JWT')
+let {method} = require('sails-async')
 
 module.exports = {
-  login: (login, username='email', password='password', id='id') => async (req, res) => {
+  login: (login, username='email', password='password', id='id') => method(async (req, res) => {
     let user = await login(req.param(username), req.param(password))
     if (user.error)
       return res.status(user.error || 401).send(user)
@@ -11,42 +12,32 @@ module.exports = {
     })
     // Set here so client auto uses it
     res.set(JWT.renewTokenHeader, token);
-    res.status(200).send({ token })
-  },
-  impersonate: (id='id') => async (req, res) => {
-    try {
-      let token = JWT.issue({
-        // The new user will be whatever the target id is
-        user: req.param(id),
-        // Add the previous user to the back of the stack of users ids
-        // Makes it easy to use with lodash :D
-        impersonateStack: [...req.tokenPayload.impersonateStack || [], req.user.id]
-      })
-      res.set(JWT.renewTokenHeader, token)
-      res.status(200).send({token})
-    } catch (e) {
-      // Return 500 because there was a problem with the server
-      // Either grabbing permissions failed (developer's fault)
-      // Or generating a token failed (server's fault)
-      res.status(500).send({message: e.message || e})
+    return {token}
+  }),
+  impersonate: (id='id') => method(async (req, res) => {
+    let token = JWT.issue({
+      // The new user will be whatever the target id is
+      user: req.param(id),
+      // Add the previous user to the back of the stack of users ids
+      // Makes it easy to use with lodash :D
+      impersonateStack: [...req.tokenPayload.impersonateStack || [], req.user.id]
+    })
+    res.set(JWT.renewTokenHeader, token)
+    return {token}
+  }),
+  impersonatePop: method(async (req, res) => {
+    let tokenRaw = {
+      // Grab last user of the stack and make the new user
+      user: _.last(req.tokenPayload.impersonateStack),
+      // New stack should be the all but the last user
+      // since they're the new user
+      impersonateStack: _.initial(req.tokenPayload.impersonateStack)
     }
-  },
-  impersonatePop: async (req, res) => {
-    try {
-      let tokenRaw = {
-        // Grab last user of the stack and make the new user
-        user: _.last(req.tokenPayload.impersonateStack),
-        // New stack should be the all but the last user
-        // since they're the new user
-        impersonateStack: _.initial(req.tokenPayload.impersonateStack)
-      }
-      let token = JWT.issue(tokenRaw)
-      res.set(JWT.renewTokenHeader, token)
-      res.status(200).send({token})
-    } catch(e) {
-      res.status(500).send({ message: e.message || e })
-    }
-  },
-  verify: (fn, verify='verify', values='values') =>
-    async req => fn(req.param('verify'), req.param('values'))
+    let token = JWT.issue(tokenRaw)
+    res.set(JWT.renewTokenHeader, token)
+    return {token}
+  })
 }
+
+module.exports.verify = (fn, verify='verify', values='values') =>
+  async req => fn(req.param('verify'), req.param('values'))
