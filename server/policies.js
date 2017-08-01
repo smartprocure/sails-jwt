947551a000d00fe6edea03168f99cde546c690d5
Sean Clayton <sclayton@smartprocure.us>
2017-08-01 15:57:41 -0400

    Throw better objects

--- server/policies.js
+++ server/policies.js
@@ -1,12 +1,14 @@
+let f = require('futil-js')
+
 module.exports = {
   impersonate: async (req, res, {id}) => {
-    if (!id || !req.tokenPayload) throw {statusCode: 403}
-    let currentUser = req.tokenPayload.user.id
+    if (!id || !req.tokenPayload) f.throws(403)
+    let currentUser = req.tokenPayload.user
     let sameUser = id === currentUser
 
-    if (sameUser) throw {statusCode: 400}
+    if (sameUser) f.throws({statusCode: 400, message: 'You cannot sign in as yourself!'})
   },
-  impersonatePop: async () => {
-    if (!req.user.isImpersonating) throw {statusCode: 400}
+  impersonatePop: async (req, res) => {
+    if (!req.user.isImpersonating) f.throws({statusCode: 400, message: 'You\'re not impersonating anyone!'})
   }
 }
