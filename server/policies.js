35f41b86a4c35e1b98faaf4179fa9b9e78c552c2
Sean Clayton <sclayton@smartprocure.us>
2017-08-01 15:45:26 -0400

    Throw object literals

--- server/policies.js
+++ server/policies.js
@@ -1,12 +1,12 @@
 module.exports = {
   impersonate: async (req, res, {id}) => {
-    if (!id || !req.tokenPayload) throw new Error('403 Forbidden')
+    if (!id || !req.tokenPayload) throw {statusCode: 403}
     let currentUser = req.tokenPayload.user.id
     let sameUser = id === currentUser
 
-    if (sameUser) throw new Error('400 Bad Request')
+    if (sameUser) throw {statusCode: 400}
   },
   impersonatePop: async () => {
-    if (!req.user.isImpersonating) throw new Error('400 Bad Request')
+    if (!req.user.isImpersonating) throw {statusCode: 400}
   }
 }
