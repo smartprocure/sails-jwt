b733508284e8d48a513dd85e6984abaa6a078df4
Sean Clayton <sclayton@smartprocure.us>
2017-08-01 11:36:13 -0400

    Add sane policies for impersonation

---
+++ server/policies.js
@@ -0,0 +1,14 @@
+let f = require('futil-js')
+
+module.exports = {
+  impersonate: async (req, res, {id}) => {
+    if (!id || !req.tokenPayload) throw new Error('403 Forbidden')
+    let currentUser = req.tokenPayload.user.id
+    let sameUser = id === currentUser
+
+    if (sameUser) throw new Error('400 Bad Request')
+  },
+  impersonatePop: async () => {
+    if (!req.user.isImpersonating) throw new Error('400 Bad Request')
+  }
+}
