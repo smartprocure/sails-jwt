5108393505f9d7645d3cca5c90a04f80f9b779ca
Giuliano Kranevitter <gkranevitter@smartprocure.us>
2017-08-23 17:14:07 -0400

    Only set the token if it has a value

--- server/stripToken.js
+++ server/stripToken.js
@@ -3,7 +3,7 @@ let JWT = require('./JWT')
 module.exports = async (req, res, next) => {
   try {
     let token = JWT.getToken(req)
-    await JWT.setTokenPayload(req, token)
+    if (token) await JWT.setTokenPayload(req, token)
     return next()
   } catch (e) {
     res.status(401).send({ message: e.message || e })
